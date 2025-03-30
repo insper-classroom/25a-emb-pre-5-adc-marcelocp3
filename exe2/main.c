f3b78e22358f977760be3c4fe8ebd028d14f0cf2
marcelocp3 <marcelocp3@al.insper.edu.br>
2025-03-30 15:13:58 -0300

    ex2

--- exe2/main.c
+++ exe2/main.c
@@ -7,15 +7,23 @@
 #include <stdio.h>
 #include "hardware/pwm.h"
 
-const int PWM_0_PIN = 4;
+const int PWM_RED_PIN = 4; 
+const int PWM_GREEN_PIN = 6;
 
 void led_task(void *p) {
-    gpio_set_function(PWM_0_PIN, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
-    pwm_set_clkdiv(slice_num, 125);
-    pwm_set_wrap(slice_num, 100);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 80);
-    pwm_set_enabled(slice_num, true);
+    gpio_set_function(PWM_RED_PIN, GPIO_FUNC_PWM);
+    uint slice_num_red = pwm_gpio_to_slice_num(PWM_RED_PIN);
+    pwm_set_clkdiv(slice_num_red, 125);
+    pwm_set_wrap(slice_num_red, 100);
+    pwm_set_chan_level(slice_num_red, PWM_CHAN_A, 80); 
+    pwm_set_enabled(slice_num_red, true);
+
+    gpio_set_function(PWM_GREEN_PIN, GPIO_FUNC_PWM);
+    uint slice_num_green = pwm_gpio_to_slice_num(PWM_GREEN_PIN);
+    pwm_set_clkdiv(slice_num_green, 125);
+    pwm_set_wrap(slice_num_green, 100);
+    pwm_set_chan_level(slice_num_green, PWM_CHAN_A, 20); 
+    pwm_set_enabled(slice_num_green, true);
 
     while (true) {
     }
